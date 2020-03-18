61a96cf028543768a7768a00711b926562e38a8e
roberto <robaraneda@gmail.com>
2020-03-18 17:49:35 -0300

    up

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,51 +1,42 @@
 <template>
   <div>
     <v-navigation-drawer
-      v-model="primaryDrawer.model"
-      :clipped="primaryDrawer.clipped"
+      v-model="drawer"
       app
       :mini-variant.sync="mini"
       permanent
     >
-      <v-list-item class="px-3 py-1">
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-title>John Leider</v-list-item-title>
-
-        <v-btn icon @click.stop="mini = !mini">
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-      </v-list-item>
-
+      <v-list>
+        <v-list-item>
+          <v-list-item-title class="title">{{ user.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle>
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+      </v-list>
       <v-divider></v-divider>
-
-      <v-list-item v-for="item in items" :key="item.text" link>
-        <v-list-item-action>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title>
-            {{ item.text }}
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <v-list nav dense>
+        <v-list-item v-for="item in items" :key="item.text" link>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      color="primary"
-      :clipped-left="primaryDrawer.clipped"
-      app
-      dark
-      flat
-    >
+    <v-app-bar color="primary" app dark flat>
       <v-toolbar-title>Vuetify</v-toolbar-title>
     </v-app-bar>
     <v-content>
       <v-container fluid> </v-container>
     </v-content>
-    <v-footer :inset="footer.inset" app>
+    <v-footer inset app>
       <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </div>
@@ -53,6 +44,7 @@
 
 <script>
 import axios from 'axios'
+import { mapGetters } from 'vuex'
 export default {
   name: 'Dashboard',
   mounted() {
@@ -60,11 +52,11 @@ export default {
       .get('//localhost:8000/api/v1/state')
       .then(response => console.log(response))
   },
+  computed: {
+    ...mapGetters(['user'])
+  },
   data: () => ({
-    primaryDrawer: {
-      model: null,
-      clipped: false
-    },
+    drawer: true,
     items: [
       { icon: 'mdi-trending-up', text: 'Most Popular' },
       { icon: 'mdi-youtube-subscription', text: 'Subscriptions' },
@@ -72,9 +64,6 @@ export default {
       { icon: 'mdi-playlist-play', text: 'Playlists' },
       { icon: 'mdi-clock', text: 'Watch Later' }
     ],
-    footer: {
-      inset: false
-    },
     mini: true
   })
 }
